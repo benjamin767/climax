9e35bd437d8b8475e033d56c111a8058c82834a6
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-30 19:58:22 -0300

    Updated NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function NavBar() {
 
     return (
         <nav className="navbar-nav bg-dark text-light mb-5">
-            <h1 className="text-light mx-auto">Climax</h1>
+            <Link to="/"><h1 className="text-light mx-auto">Climax</h1></Link>
         </nav>
     );
 }
