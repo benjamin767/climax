9b1f34a6330ff4f7ef28fbc63139ab12a806a38c
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-30 20:09:00 -0300

    Updated Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import CardsContainer from "../CardsContainer/CardsContainer";
 import SearchBar from "../SearchBar/SearchBar";
-import "./Home.css"
 
 export default function Home({ onSearch, cities, onClose }) {
    
     return (
         <>
             <SearchBar onSearch={onSearch}/>
-            <div className="home_container">
+            <div className="d-flex flex-row justify-content-center flex-wrap">
                 <CardsContainer cities={cities} onClose={onClose}/> 
             </div>
         </>
