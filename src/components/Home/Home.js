2db18f10440b83a99ca91418a53d05029e993b2b
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-30 20:54:34 -0300

    Updated Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,11 +5,11 @@ import SearchBar from "../SearchBar/SearchBar";
 export default function Home({ onSearch, cities, onClose }) {
    
     return (
-        <>
+        <section className="vh-75">
             <SearchBar onSearch={onSearch}/>
             <div className="d-flex flex-row justify-content-center flex-wrap">
                 <CardsContainer cities={cities} onClose={onClose}/> 
             </div>
-        </>
+        </section>
     );
 }
