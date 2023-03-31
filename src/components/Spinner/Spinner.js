954c62885d11f7d1a8b0b4eb946d747b9bd5f314
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-31 12:17:14 -0300

    Updated Spinner.js

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import "./Spinner.css";
 
 export default function Spinner() {
     return (
-        <div class="loadingio-spinner-double-ring-lobirv6fz9p vh-100"><div class="ldio-drhb5h4ulee">
+        <div class="loadingio-spinner-double-ring-lobirv6fz9p vh-75"><div class="ldio-drhb5h4ulee">
             <div></div>
             <div></div>
             <div><div></div></div>
