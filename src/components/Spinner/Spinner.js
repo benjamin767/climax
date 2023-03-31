10e9f186fc3a36924bb6bc82746a4339b774a311
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-30 21:04:40 -0300

    added Spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Spinner() {
+    return (
+        <div class="loadingio-spinner-double-ring-lobirv6fz9p"><div class="ldio-drhb5h4ulee">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    );
+}
