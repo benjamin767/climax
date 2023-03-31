3a3978de45ba26874f0c2b93e1490b91ca3cc8ac
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-30 21:34:51 -0300

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
 import CardsContainer from "../CardsContainer/CardsContainer";
 import SearchBar from "../SearchBar/SearchBar";
+import Spinner from "../Spinner/Spinner";
 
 export default function Home({ onSearch, cities, onClose }) {
-   
+
     return (
         <section className="vh-75">
-            <SearchBar onSearch={onSearch}/>
+            <SearchBar onSearch={onSearch} />
             <div className="d-flex flex-row justify-content-center flex-wrap">
-                <CardsContainer cities={cities} onClose={onClose}/> 
+                {cities.length
+                    ? <CardsContainer cities={cities} onClose={onClose} />
+                    : <Spinner />}
             </div>
         </section>
     );
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Spinner.css";
 
 export default function Spinner() {
     return (
-        <div class="loadingio-spinner-double-ring-lobirv6fz9p"><div class="ldio-drhb5h4ulee">
+        <div class="loadingio-spinner-double-ring-lobirv6fz9p vh-100"><div class="ldio-drhb5h4ulee">
             <div></div>
             <div></div>
             <div><div></div></div>
